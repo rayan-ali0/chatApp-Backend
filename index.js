28e9f89af7c4cda24794330681dcf9d1f8d3f54c
Rayan ALi <rayan.ali.8205@gmail.com>
2023-12-06 23:43:51 +0200

    socket done

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+import express  from "express";
+import http from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+const app=express()
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  // options
-});
+app.use(cors())
 
-io.on("connection", (socket) => {
-  // ...
-});
+const server=http.createServer(app)
+const io=new Server(server,{
+  cors:{
+    origin:"http://localhost:3000",
+    methods:["GET","POST"]
+  }
+})
 
-httpServer.listen(3000);
+app.get('/',(req,res)=>{
+  res.json("hii")
+})
+
+io.on('connection',(socket)=>{
+  console.log("User : "+socket.id);
+
+  socket.on("message",(data)=>{
+    console.log(data);
+    socket.broadcast.emit('message',data)
+
+  })
+})
+
+server.listen(5000,()=>{
+  console.log("Server running...")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import { createServer } from "http";
+// import { Server } from "socket.io";
+// import express from 'express';
+// import { createServer } from 'node:http';
+// import { fileURLToPath } from 'node:url';
+// import { dirname, join } from 'node:path';
+// const io = new Server(server);
+
+
+// const app = express();
+// const server = createServer(app);
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//   });
+  
+// server.listen(5000, () => {
+//   console.log('server running at http://localhost:5000');
+// });
