c44eafbfef7029dafed8d1ee971739682c273e67
Fuad Abou Heit <f.abouheit@gmail.com>
2023-12-07 08:02:30 +0200

    fixed bug

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 import express from "express";
+import http from "http"; // import http module to create a server
 import dotenv from 'dotenv';
-// import { createServer } from "http";
-import { Server } from 'socket.io'
+import cors from 'cors';
+import { Server } from 'socket.io';
+
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-const PORT = 6000;
-
-app.use(
-    cors({
-      origin: process.env.SERVER_PATH,
-    })
-  );
-
-app.on('connection', socket =>{
-    console.log(`User ${socket.id} connected`);
-    socket.on('message', data=>{
-        console.log(data);
-        app.emit('message', `${socket.id.substring(0,5)}: ${data}`)
-    })
-})
-
-app.listen(process.env.PORT, () => {
-    console.log("listening on port: ", process.env.PORT);
+const server = http.createServer(app); // create an HTTP server using express app
+const io = new Server(server, {
+  cors: {
+    // origin: "http://localhost:3003",
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
+// const PORT = process.env.PORT || 6000;
 
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on('message', (data) => {
+    console.log(data);
+    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} disconnected`);
+  });
+});
+
+io.listen(7778);
+// server.listen(PORT, () => {
+//   console.log("Server is listening on port:", PORT);
+// });
